f623ac878975cbf50293b98ba85d6e93069794ca
umairkhan01 <72546198+umairkhan01@users.noreply.github.com>
2023-03-30 22:45:26 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,33 +1,25 @@
-const contactForm = document.getElementById("contact-form");
-const formResult = document.getElementById("form-result");
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', handleSubmit);
 
-contactForm.addEventListener("submit", function(event) {
+function handleSubmit(event) {
   event.preventDefault();
-
-  const title = document.getElementById("title").value;
-  const firstName = document.getElementById("first-name").value;
-  const lastName = document.getElementById("last-name").value;
-  const dob = document.getElementById("dob").value;
-  const telephone = document.getElementById("telephone").value;
-  const fileUpload = document.getElementById("file-upload").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  let hasError = false;
-
-  if (!title || !firstName || !lastName || !telephone || !email) {
-    hasError = true;
-  }
-
-  if (hasError) {
-    formResult.innerHTML = "Please fill out all required fields.";
-    formResult.classList.add("error");
-    return;
-  }
-
-  formResult.innerHTML = `Thank you ${title} ${firstName} ${lastName}.<br>You have submitted the following details:<br>Date of Birth: ${dob}<br>Telephone: ${telephone}<br>File Uploaded: ${fileUpload}<br>Message: ${message}`;
-  formResult.classList.remove("error");
-
+  const formData = new FormData(event.target);
+  const title = formData.get('title');
+  const firstName = formData.get('first-name');
+  const lastName = formData.get('last-name');
+  const dob = formData.get('dob');
+  const telephone = formData.get('telephone');
+  const fileUpload = formData.get('file-upload');
+  const email = formData.get('email');
+  const message = formData.get('message');
+  
+  // Do form validation here
+  
+  const messageEl = document.createElement('p');
+  messageEl.textContent = `Thank you ${title} ${firstName} ${lastName}.\nYou have submitted the following details:\nDate of Birth: ${dob}\nTelephone: ${telephone}\nFile Uploaded: ${fileUpload}\nMessage: ${message}`;
+  
+  // Display message on next screen here
+  
   contactForm.reset();
-});
+}
 
