848cb34014269f24867cf11408bb50187955d9fe
umairkhan01 <72546198+umairkhan01@users.noreply.github.com>
2023-03-31 01:14:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Contact Form Details</title>
-  </head>
-  <body>
-    <h2>Contact Form Details</h2>
-    <?php
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $message = $_POST['message'];
-      $gender = $_POST['gender'];
-      $country = $_POST['country'];
+const form = document.getElementById('contact-form');
 
-      echo "<p><strong>Name:</strong> $name</p>";
-      echo "<p><strong>Email:</strong> $email</p>";
-      echo "<p><strong>Message:</strong> $message</p>";
-      echo "<p><strong>Gender:</strong> $gender</p>";
-      echo "<p><strong>Country:</strong> $country</p>";
-    ?>
-  </body>
-</html>
+function handleSubmit(event) {
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const dob = document.getElementById('dob').value;
+  const phone = document.getElementById('phone').value;
+  const file = document.getElementById('file').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Create an object to hold the form data
+  const formData = {
+    'Title': title,
+    'First Name': firstName,
+    'Last Name': lastName,
+    'Date of Birth': dob,
+    'Telephone Number': phone,
+    'File Uploaded': file,
+    'Email': email,
+    'Message': message
+  };
+
+  // Store the form data in sessionStorage
+  sessionStorage.setItem('formData', JSON.stringify(formData));
+
+  // Redirect to the confirmation page
+  window.location = 'index.html';
+}
+
+form.addEventListener('submit', handleSubmit);
